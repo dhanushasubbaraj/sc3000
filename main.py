bfebc30747431fb77c8724aec5adae9f16f912e6
Poojablessy-2003 <kavipoojantu@gmail.com>
2026-03-08 17:50:05 +0800

    Main file - one run to view both parts

---
+++ main.py
@@ -0,0 +1,8 @@
+from part2_grid import pt2_gridworld
+
+def main():
+    print ("Part 2: Solving MDP and Reinforcement Learning Problems Using a Grid World")
+    pt2_gridworld()
+
+if __name__ == "__main__":
+    main()
