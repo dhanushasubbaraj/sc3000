fc931d14f7322a3e9a0f00775e14679db65171de
dhanushasubbaraj <dhanusha529@gmail.com>
2026-03-09 00:37:13 +0800

    task3

--- task1/t1main.py
+++ task1/t1main.py
@@ -1,6 +1,7 @@
 import json
 import task1
 import task2
+import task3
 
 
 # Load JSON files
@@ -22,3 +23,6 @@ print("Task 2:")
 print()
 task2.UCS_energy("1", "50", G, Dist, Cost)
 
+print("Task 3:")
+print()
+task3.A_star("1", "50", G, Dist, Cost, Coord)
